d179a8cb1fb9c29358f5869dee9082e372440793
PhucLe03 <hailua112003@gmail.com>
2023-11-23 19:56:41 +0700

    Minor fix

--- cart/index.php
+++ cart/index.php
@@ -6,130 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
         <?php
-        $title = "BK ECommerce - Home";
-        include "header.php";
+        $title = "Cart";
+        include "../header.php";
         ?>
     </title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="icon" href="../assets/images/logo.png">
+    <?php include "../assets/global/global_css.php"?>
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
-    <?php include "../global_nav.php" ?>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script> -->
+    <?php include "../assets/global/global_nav.php";?>
     <div class=" container">
-        <div class="slider">
-            
-        </div>
-    </div>
-    <div class="product text-md-left">
-        <div class="row  text-md-left">
-            <div class="col-md-1 col-lg-1 col-xl-1 mx-auto mt-3">
-                <p><img src="./assets/images/quangcao1.png" alt="Advertisement 1"></p>
-            </div>
-            <div class="col-md-10 col-lg-10 col-xl-10 mx-auto mt-3">
-                <div class="overflow-hidden">
-                    <h1 text-uppercase mb-4 font-weight-bold text-warning>PC bán chạy</h1>
-                    <div class="row gy-5">
-                        <!-- product 1 -->
-                        <div class="col-6 p-3 ">
-                            <div class="row">
-                                <div class="col-md-4 ">
-                                    <img src="./assets/images/pc.png" alt="this is logo" height="150" width="150">
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="newProduct">
-                                        Sản phẩm mới
-                                    </div>
-                                    <div class="nameOfProduct">
-                                        {{product}}
-                                    </div>
-                                    <div class="accessoryOfProduct">
-                                        {{accessory}}
-                                    </div>
-                                    <div class="priceOfProduct">
-                                        {{price}}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- product 2 -->
-                        <div class="col-6 p-3 ">
-                            <div class="row">
-                                <div class="col-md-4 ">
-                                    <img src="./assets/images/pc.png" alt="this is logo" height="150" width="150">
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="newProduct">
-                                        Sản phẩm mới
-                                    </div>
-                                    <div class="nameOfProduct">
-                                        {{product}}
-                                    </div>
-                                    <div class="accessoryOfProduct">
-                                        {{accessory}}
-                                    </div>
-                                    <div class="priceOfProduct">
-                                        {{price}}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- product 3 -->
-                        <a class="col-6 p-3" href="/cart">
-                            <div>
-                                <div class="row">
-                                    <div class="col-md-4 ">
-                                        <img src="./assets/images/pc.png" alt="this is logo" height="150" width="150">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="newProduct">
-                                            Sản phẩm mới
-                                        </div>
-                                        <div class="nameOfProduct">
-                                            {{product}}
-                                        </div>
-                                        <div class="accessoryOfProduct">
-                                            {{accessory}}
-                                        </div>
-                                        <div class="priceOfProduct">
-                                            {{price}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a class="col-6 p-3" href="/cart">
-                            <div>
-                                <div class="row">
-                                    <div class="col-md-4 ">
-                                        <img src="./assets/images/pc.png" alt="this is logo" height="150" width="150">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="newProduct">
-                                            Sản phẩm mới
-                                        </div>
-                                        <div class="nameOfProduct">
-                                            {{product}}
-                                        </div>
-                                        <div class="accessoryOfProduct">
-                                            {{accessory}}
-                                        </div>
-                                        <div class="priceOfProduct">
-                                            {{price}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-1 col-lg-10 col-xl-1 mx-auto mt-3">
-                <p><img src="./assets/images/quangcao2.png" alt="Advertisement 2"></p>
-            </div>
-        </div>
+        <h1>Cart View</h1>
+        <div class="slider"></div>
     </div>
 </body>
 
